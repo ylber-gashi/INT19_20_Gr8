bc405275684d100f74357a7f9019ea2b17255820
rrustemh <58404371+rrustemh@users.noreply.github.com>
2019-12-21 23:16:00 +0100

    Update slider.js

--- slider.js
+++ slider.js
@@ -55,13 +55,5 @@
         $(document).ready(function(){
            slider.init();
           });
-        /*Kodi per background color qe ndryshon gjate koheve te ndryshme te dites*/
-         var t = new Date().getHours();
-         if (t>5 && t <= 10 ) {
-             document.body.style.backgroundColor="yellow";
-         }else if (t>10 && t <= 20) {
-             document.body.style.backgroundColor="orange";
-         }else {
-             document.body.style.backgroundColor="grey";
-         }
+       
  
